557bc0455c07a1dadc90aec8cbf642c412419eae
andrew.andrews <andrewardyour@gmail.com>
2026-01-16 19:27:33 +0500

    update

--- src/config/settings.py
+++ src/config/settings.py
@@ -66,11 +66,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST", "db"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "NAME": os.getenv("POSTGRES_DB", "course_work"),
+        "USER": os.getenv("POSTGRES_USER", "course_work"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "course_work"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
